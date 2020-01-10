72995c5be9dc8fccf8b49ad43e8e87fa5acfcfcf
Imraan Ali <imraan91@hotmail.co.uk>
2020-01-10 22:58:17 +0000

    can run app file and show content basic test passing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,20 @@
-require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara/rspec'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
   # SimpleCov::Formatter::HTMLFormatter
-])
+  ])
 SimpleCov.start
 
-# For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
+ENV['RACK_ENV'] = 'test'
 
-
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
+require 'capybara/rspec'
+Capybara.app = Rps
 
 RSpec.configure do |config|
   config.after(:suite) do
